4fbf7d03bbb3aff4aff1448dbc1a7a196439bbc3
imom29 <omtita971@gmail.com>
2024-02-02 12:36:18 +0530

    Relation Between Watchlist and platform

--- moviemil/watchlist_app/models.py
+++ moviemil/watchlist_app/models.py
@@ -5,6 +5,9 @@ class Watchlist(models.Model):
     title = models.CharField(max_length=50)
     storyline = models.CharField(max_length=200)
     active = models.BooleanField(default=True)
+    platform = models.ForeignKey(
+        "StreamPlatform", on_delete=models.CASCADE, related_name="watchlist"
+    )
 
     def __str__(self):
         return self.title
